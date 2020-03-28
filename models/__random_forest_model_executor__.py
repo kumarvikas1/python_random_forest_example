ba0230a8f1a73afcdebec0f8384cea7e8dfc2cb7
vikas kumar <vikakumar@expediagroup.com>
2020-03-28 21:43:46 +0530

    Load Model on init

--- models/__random_forest_model_executor__.py
+++ models/__random_forest_model_executor__.py
@@ -8,9 +8,14 @@ from abc import ABC, abstractmethod
 from sanic.response import json
 
 class RandomForestRegression(ModelExecutor) :
+
+    model = ''
+    def __init__(self):
+        self.model = lib.load("/opt/models/random_forest/test_model_save.pkl")
+
+
     def executeModel(self, request):
-        model = lib.load("/opt/models/random_forest/test_model_save.pkl")
         #features_predict= pd.read_csv('/Users/vikakumar/Desktop/test_ml_ola_predict.csv')
         req = pd.read_json(request.body)
         #print(features_predict.to_json(orient='records'))
-        return model.predict(req).tolist()
+        return self.model.predict(req).tolist()
